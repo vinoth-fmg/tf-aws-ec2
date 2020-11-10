66ca114f7a561f2f8170d1effe0b1dd898e3ac7f
vinoth-fmg <58342603+vinoth-fmg@users.noreply.github.com>
2020-11-10 15:43:04 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,5 @@
 resource "aws_instance" "aws_instance_devops" {
-  instance_type = "t2.micro"
- // ami           = "ami-0083662ba17882949"
-  ami           = "IMGID" 
-
-  # This will create 4 instances
-  count = 2
+  instance_type = var.instancetype
+  ami           = var.imageid 
+  count = var.count
 }
