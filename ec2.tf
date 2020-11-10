3d60db23f605d807be21803e3cfed3452a474d59
vinoth-fmg <58342603+vinoth-fmg@users.noreply.github.com>
2020-11-10 16:15:34 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,4 +2,9 @@ resource "aws_instance" "aws_instance_devops" {
   instance_type = var.instancetype
   ami           = var.imageid 
   count = var.instancecount
+  subnet_id = var.subnetid
+  
+  tags = {
+    Name = "DEVOPS-DEMO-EC2"
+  }
 }
